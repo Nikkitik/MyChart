b6bc8639d6d1727d8cb6d9755e2b274b54565199
nikkitik <nikkitik21@gmail.com>
2016-12-23 15:58:13 +0300

    3 цвета для разных типов

---
+++ app/src/main/java/com/example/nromantsov/mychart/MyColor.java
@@ -0,0 +1,13 @@
+package com.example.nromantsov.mychart;
+
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+/**
+ * Created by n.romantsov on 23.12.2016.
+ */
+
+public class MyColor extends ColorTemplate {
+    public static final int[] THREE_COLORS = {
+            rgb("#2ecc71"), rgb("#f1c40f"), rgb("#e74c3c")
+    };
+}
