f7624cfeb97f90b9b66e470d5b37734db539d13d
nikkitik <nikkitik21@gmail.com>
2017-01-13 14:55:44 +0300

    добавлены цвета

--- app/src/main/java/com/example/nromantsov/mychart/MyColor.java
+++ app/src/main/java/com/example/nromantsov/mychart/MyColor.java
@@ -1,5 +1,7 @@
 package com.example.nromantsov.mychart;
 
+import android.graphics.Color;
+
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 /**
@@ -7,9 +9,15 @@ import com.github.mikephil.charting.utils.ColorTemplate;
  */
 
 public class MyColor extends ColorTemplate {
-    public static final int[] THREE_COLORS = {
-            rgb("#2ecc71"), rgb("#f1c40f"), rgb("#e74c3c")
+    public static final int[] THREES_COLORS = {
+            rgb("#006569"), rgb("#8ad9db"), rgb("#009ba1")
+//            Color.rgb(0, 101, 105), Color.rgb(138, 217, 219), Color.rgb(0, 155, 161)
     };
 
-    public static final int WHITE_COLORS = rgb("#574eff");
+    public static final int WHITE_COLORS = rgb("#FAFAFA");
+    public static final int BROWN_COLORS = rgb("#FF5C2200");
+
+    public static final int ONE_COLORS = rgb("#006569");
+    public static final int TWO_COLORS = rgb("#8ad9db");
+    public static final int THREE_COLORS = rgb("#009ba1");
 }
