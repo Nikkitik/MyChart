3d89f54dd916252db1d923d77f1f133fd647b897
nikkitik <nikkitik21@gmail.com>
2017-01-16 14:51:57 +0300

    интерфейс для ручной записи цветов и радиуса

---
+++ app/src/main/java/com/example/nromantsov/mychart/IRender.java
@@ -0,0 +1,10 @@
+package com.example.nromantsov.mychart;
+
+/**
+ * Created by n.romantsov on 16.01.2017.
+ */
+
+public interface IRender {
+    void setColor(int[] colors);
+    void setRadius(float radius);
+}
